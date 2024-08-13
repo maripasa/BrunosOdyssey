9761ea9c27cd401a2f4eef01f31e625bf14aad32
asa <maripasa@proton.me>
2024-08-13 16:35:05 -0300

    Background work

--- Hitbox.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body):
-	if body.name == "player":
-		body.velocity.y = body.JUMP_VELOCITY
-		owner.anim.play("hurt")
--- jogo teste/cereja.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-func _process(_delta):
-	
-	var direction = Input.get_vector("Left", "Right","Jump", "Climb")
-	velocity = direction * 500
-	move_and_slide() 
-	
