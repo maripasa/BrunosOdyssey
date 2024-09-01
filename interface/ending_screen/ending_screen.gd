005b56d88147771e2c9144fcf4ba0683db7862c2
maripasa <main.grudge573@aleeas.com>
2024-09-01 16:14:42 -0300

    fix(PJ003): Hotfixes

--- interface/ending_screen/ending_screen.gd
+++ interface/ending_screen/ending_screen.gd
@@ -1,4 +1,4 @@
-extends Control
+extends CanvasLayer
 class_name MainMenu
 
 @export_category("Objects")
--- interface/menu/main_menu.gd
+++ interface/menu/main_menu.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Control
 class_name EndingScreen
 
 @export_category("Objects")
